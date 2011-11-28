66a5e264b189a5c9aaebf6462672789dd3660c36
SumiTomohiko <SumiTomohiko@neko-daisuki.ddo.jp>
2011-11-29 02:08:06 +0900

    Arranged code format to my favor.

--- src/main.c
+++ src/main.c
@@ -1,58 +1,67 @@
-/* TinyWM is written by Nick Welch <mack@incise.org>, 2005.
- *
- * This software is in the public domain
- * and is provided AS IS, with NO WARRANTY. */
-
+#include <stdio.h>
+#include <strings.h>
 #include <X11/Xlib.h>
 
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MAX(a, b) ((b) < (a) ? (a) : (b))
 
-int main()
+static void
+print_error(const char* msg)
 {
-    Display * dpy;
-    Window root;
-    XWindowAttributes attr;
-    XButtonEvent start;
-    XEvent ev;
+    fprintf(stderr, "%s\n", msg);
+}
 
-    if(!(dpy = XOpenDisplay(0x0))) return 1;
+int
+main(int argc, const char* argv[])
+{
+    Display* display = XOpenDisplay(NULL);
+    if (display == NULL) {
+        print_error("Cannot open display");
+        return 1;
+    }
 
-    root = DefaultRootWindow(dpy);
+    Window root = DefaultRootWindow(display);
 
-    XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym("F1")), Mod1Mask, root,
+#if 0
+    XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("F1")), Mod1Mask, root,
             True, GrabModeAsync, GrabModeAsync);
-    XGrabButton(dpy, 1, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync,
-            GrabModeAsync, None, None);
-    XGrabButton(dpy, 3, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync,
-            GrabModeAsync, None, None);
+#endif
+    XGrabButton(display, 1, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
+    XGrabButton(display, 3, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
 
-    for(;;)
-    {
-        XNextEvent(dpy, &ev);
-        if(ev.type == KeyPress && ev.xkey.subwindow != None)
-            XRaiseWindow(dpy, ev.xkey.subwindow);
-        else if(ev.type == ButtonPress && ev.xbutton.subwindow != None)
-        {
-            XGrabPointer(dpy, ev.xbutton.subwindow, True,
+    XButtonEvent start;
+    bzero(&start, sizeof(start));
+    XWindowAttributes attr;
+    bzero(&attr, sizeof(attr));
+    while (1) {
+        XEvent ev;
+        XNextEvent(display, &ev);
+        if ((ev.type == KeyPress) && (ev.xkey.subwindow != None)) {
+            XRaiseWindow(display, ev.xkey.subwindow);
+        }
+        else if ((ev.type == ButtonPress) && (ev.xbutton.subwindow != None)) {
+            XGrabPointer(display, ev.xbutton.subwindow, True,
                     PointerMotionMask|ButtonReleaseMask, GrabModeAsync,
                     GrabModeAsync, None, None, CurrentTime);
-            XGetWindowAttributes(dpy, ev.xbutton.subwindow, &attr);
+            XGetWindowAttributes(display, ev.xbutton.subwindow, &attr);
             start = ev.xbutton;
         }
-        else if(ev.type == MotionNotify)
-        {
-            int xdiff, ydiff;
-            while(XCheckTypedEvent(dpy, MotionNotify, &ev));
-            xdiff = ev.xbutton.x_root - start.x_root;
-            ydiff = ev.xbutton.y_root - start.y_root;
-            XMoveResizeWindow(dpy, ev.xmotion.window,
-                attr.x + (start.button==1 ? xdiff : 0),
-                attr.y + (start.button==1 ? ydiff : 0),
-                MAX(1, attr.width + (start.button==3 ? xdiff : 0)),
-                MAX(1, attr.height + (start.button==3 ? ydiff : 0)));
+        else if (ev.type == MotionNotify) {
+            while (XCheckTypedEvent(display, MotionNotify, &ev)) {
+            }
+            int xdiff = ev.xbutton.x_root - start.x_root;
+            int ydiff = ev.xbutton.y_root - start.y_root;
+            XMoveResizeWindow(display, ev.xmotion.window,
+                attr.x + (start.button == 1 ? xdiff : 0),
+                attr.y + (start.button == 1 ? ydiff : 0),
+                MAX(1, attr.width + (start.button == 3 ? xdiff : 0)),
+                MAX(1, attr.height + (start.button == 3 ? ydiff : 0)));
         }
-        else if(ev.type == ButtonRelease)
-            XUngrabPointer(dpy, CurrentTime);
+        else if (ev.type == ButtonRelease) {
+            XUngrabPointer(display, CurrentTime);
+        }
     }
 }
 
+/**
+ * vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
+ */
