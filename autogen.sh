1f26d0cae68fbffdfe6038b06c71a6c777578d4d
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2011-12-07 18:11:59 +0900

    Implemented from scratch, not based on TinyWM.

--- autogen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-autoreconf --install
-
-# vim: tabstop=2 shiftwidth=2 expandtab softtabstop=2
--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-#include <strings.h>
 #include <X11/Xlib.h>
 
-#define MAX(a, b) ((b) < (a) ? (a) : (b))
+struct WindowManager {
+    Display* display;
+};
+
+typedef struct WindowManager WindowManager;
 
 static void
 print_error(const char* msg)
@@ -10,56 +13,83 @@ print_error(const char* msg)
     fprintf(stderr, "%s\n", msg);
 }
 
+static void
+reparent_window(WindowManager* wm, Window w)
+{
+    Display* display = wm->display;
+    int screen = DefaultScreen(display);
+    XWindowAttributes wa;
+    XGetWindowAttributes(display, w, &wa);
+    int border_size = 4;
+    int title_height = 32;
+    Window frame = XCreateSimpleWindow(
+        display,
+        DefaultRootWindow(display),
+        wa.x, wa.y,
+        wa.width + 2 * border_size, wa.height + title_height + border_size,
+        0,
+        BlackPixel(display, screen), WhitePixel(display, screen));
+    XReparentWindow(display, w, frame, border_size, title_height);
+    XMapWindow(display, frame);
+    XMapWindow(display, w);
+    XAddToSaveSet(display, w);
+}
+
+static void
+reparent_toplevels(WindowManager* wm)
+{
+    Display* display = wm->display;
+    Window root = DefaultRootWindow(display);
+    Window _;
+    Window* children;
+    unsigned int nchildren;
+    if (XQueryTree(display, root, &_, &_, &children, &nchildren) == 0) {
+        print_error("XQueryTree failed");
+        return;
+    }
+    int i;
+    for (i = 0; i < nchildren; i++) {
+        XWindowAttributes wa;
+        XGetWindowAttributes(display, children[i], &wa);
+        if (IsUnmapped == wa.map_state) {
+            continue;
+        }
+        reparent_window(wm, children[i]);
+    }
+}
+
+static void
+wm_main(WindowManager* wm)
+{
+    reparent_toplevels(wm);
+    Display* display = wm->display;
+    XSelectInput(display, DefaultRootWindow(display), SubstructureRedirectMask);
+
+    while (1) {
+        XEvent e;
+        XNextEvent(wm->display, &e);
+        if (e.type == MapRequest) {
+            reparent_window(wm, e.xmaprequest.window);
+        }
+    }
+}
+
 int
 main(int argc, const char* argv[])
 {
     Display* display = XOpenDisplay(NULL);
     if (display == NULL) {
-        print_error("Cannot open display");
+        print_error("XOpenDisplay failed.");
         return 1;
     }
 
-    Window root = DefaultRootWindow(display);
+    WindowManager wm;
+    wm.display = display;
+    wm_main(&wm);
 
-#if 0
-    XGrabKey(display, XKeysymToKeycode(display, XStringToKeysym("F1")), Mod1Mask, root,
-            True, GrabModeAsync, GrabModeAsync);
-#endif
-    XGrabButton(display, 1, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
-    XGrabButton(display, 3, Mod1Mask, root, True, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, None);
+    XCloseDisplay(display);
 
-    XButtonEvent start;
-    bzero(&start, sizeof(start));
-    XWindowAttributes attr;
-    bzero(&attr, sizeof(attr));
-    while (1) {
-        XEvent ev;
-        XNextEvent(display, &ev);
-        if ((ev.type == KeyPress) && (ev.xkey.subwindow != None)) {
-            XRaiseWindow(display, ev.xkey.subwindow);
-        }
-        else if ((ev.type == ButtonPress) && (ev.xbutton.subwindow != None)) {
-            XGrabPointer(display, ev.xbutton.subwindow, True,
-                    PointerMotionMask|ButtonReleaseMask, GrabModeAsync,
-                    GrabModeAsync, None, None, CurrentTime);
-            XGetWindowAttributes(display, ev.xbutton.subwindow, &attr);
-            start = ev.xbutton;
-        }
-        else if (ev.type == MotionNotify) {
-            while (XCheckTypedEvent(display, MotionNotify, &ev)) {
-            }
-            int xdiff = ev.xbutton.x_root - start.x_root;
-            int ydiff = ev.xbutton.y_root - start.y_root;
-            XMoveResizeWindow(display, ev.xmotion.window,
-                attr.x + (start.button == 1 ? xdiff : 0),
-                attr.y + (start.button == 1 ? ydiff : 0),
-                MAX(1, attr.width + (start.button == 3 ? xdiff : 0)),
-                MAX(1, attr.height + (start.button == 3 ? ydiff : 0)));
-        }
-        else if (ev.type == ButtonRelease) {
-            XUngrabPointer(display, CurrentTime);
-        }
-    }
+    return 0;
 }
 
 /**
